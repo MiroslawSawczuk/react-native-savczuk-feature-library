8c873a2489539f1f518dce1f935bb89f289724bb
MirekSpyroSH <Miroslaw.Sawczuk@simplyhealth.co.uk>
2022-12-26 14:13:46 +0100

    feat: add sum function

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,6 @@
-export function multiply(a: number, b: number): Promise<number> {
+export const multiply = (a: number, b: number): Promise<number> => {
   return Promise.resolve(a * b);
-}
+};
+export const sum = (a: number, b: number): Promise<number> => {
+  return Promise.resolve(a + b);
+};
